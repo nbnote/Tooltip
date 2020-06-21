1cd4b49c4d33a1d5be81570edd5b61d4f83a9795
Kazuhiro Shintani <ks@KazuhironoMacBook-puro.local>
2020-06-21 17:57:18 +0900

    update
    見切れる場合に吹き出し表示位置を自動調整

--- src/main.ts
+++ src/main.ts
@@ -3,39 +3,154 @@ import "./style.scss";
 import $ from "jquery";
 
 $(() => {
+    function capitalize(word:string):string {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+
     $('[data-tooltip-content]')
         .each((index:number, element:HTMLElement) => {
             const $body:JQuery = $('body');
             const $target:JQuery = $(element);
             const id:string = 'tooltip-' + index;
+            let position:string;
             let content:string = $target.attr('data-tooltip-content');
             let $contentWrap:JQuery = $('<div class="tooltipContentWrap"></div>');
             let $contentBg:JQuery = $('<div class="tooltipContentBg"></div>');
             let $content:JQuery;
+
+            function inView(offsetTop:number, offsetLeft:number, width:number, height:number):boolean {
+                let inView:boolean = false;
+
+                const $window:JQuery = $(window as any);
+                const scrollTop:number = $window.scrollTop();
+                const scrollBottom:number = scrollTop + $window.height()
+                const scrollLeft:number = $window.scrollLeft();
+                const scrollRight:number = scrollLeft + $window.width();
+
+                if (scrollBottom > offsetTop + height && scrollTop < offsetTop) {
+                    if (scrollRight > offsetLeft + width && scrollLeft < offsetLeft) {
+                        inView = true;
+                    }
+                }
+
+                return inView;
+            }
 
             function show(e:Event):void {
                 $target.attr('aria-describedby', id);
 
                 $contentWrap.appendTo($body);
 
-                $contentBg
-                    .css({
-                        width: $content.outerWidth(),
-                        height: $content.outerHeight() + $target.outerHeight() + 10
+                const width:number = $content.outerWidth();
+                const height:number = $content.outerHeight();
+
+                const posTop:{ [s:string]:number } = {
+                    top: $target.offset().top + $target.outerHeight() - (height + $target.outerHeight() + 10),
+                    left: $target.offset().left + ($target.outerWidth() / 2) - (width / 2),
+                    width: width,
+                    height: height + $target.outerHeight() + 10
+                };
+
+                const posBottom:{ [s:string]:number } = {
+                    top: $target.offset().top,
+                    left: posTop.left,
+                    width: posTop.width,
+                    height: posTop.height
+                };
+
+                const posLeft:{ [s:string]:number } = {
+                    top: $target.offset().top + ($target.outerHeight() / 2) - (height / 2),
+                    left: $target.offset().left - (width + 10),
+                    width: width + $target.outerWidth() + 10,
+                    height: height
+                };
+
+                const posRight:{ [s:string]:number } = {
+                    top: posLeft.top,
+                    left: $target.offset().left,
+                    width: posLeft.width,
+                    height: height
+                };
+
+                if (inView(posTop.top, posTop.left, posTop.width, posTop.height)) {
+                    position = 'top';
+                } else if (inView(posBottom.top, posBottom.left, posBottom.width, posBottom.height)) {
+                    position = 'bottom';
+                } else if (inView(posLeft.top, posLeft.left, posLeft.width, posLeft.height)) {
+                    position = 'left';
+                } else if (inView(posRight.top, posRight.left, posRight.width, posRight.height)) {
+                    position = 'right';
+                } else {
+                    position = 'top';
+                }
+
+                $content.addClass('tooltipContent--' + position);
+
+                if (position === 'top') {
+                    $contentBg.css({
+                        width: posTop.width,
+                        height: posTop.height
                     });
 
+                    $contentWrap
+                        .css({
+                            top: posTop.top,
+                            left: posTop.left,
+                            width: posTop.width,
+                            height: posTop.height
+                        });
+                } else if (position === 'bottom') {
+                    $contentBg
+                        .css({
+                            width: posBottom.width,
+                            height: posBottom.height
+                        });
+
+                    $contentWrap
+                        .css({
+                            top: posBottom.top,
+                            left: posBottom.left,
+                            width: posBottom.width,
+                            height: posBottom.height
+                        });
+                } else if (position === 'left') {
+                    $contentBg
+                        .css({
+                            width: posLeft.width,
+                            height: posLeft.height
+                        });
+
+                    $contentWrap
+                        .css({
+                            top: posLeft.top,
+                            left: posLeft.left,
+                            width: posLeft.width,
+                            height: posLeft.height
+                        });
+                } else {
+                    $contentBg
+                        .css({
+                            width: posRight.width,
+                            height: posRight.height
+                        });
+
+                    $contentWrap
+                        .css({
+                            top: posRight.top,
+                            left: posRight.left,
+                            width: posRight.width,
+                            height: posRight.height
+                        });
+                }
+
                 $contentWrap
-                    .css({
-                        top: $target.offset().top + $target.outerHeight() - $contentBg.height(),
-                        left: $target.offset().left + ($target.outerWidth() / 2) - ($contentBg.outerWidth() / 2)
-                    })
                     .on('animationend.tooltip', () => {
                         $contentWrap
-                            .removeClass('tooltipShowAnim')
+                            .removeClass('tooltipShowAnim' + capitalize(position))
                             .off('animationend.tooltip');
                     })
                     .attr('id', id)
-                    .addClass('tooltipShowAnim');
+                    .addClass('tooltipShowAnim' + capitalize(position));
 
                 $body.on('mousemove.tooltip-' + index, (e:Event) => {
                     let $eventTarget:JQuery = $(e.target as HTMLElement);
@@ -65,14 +180,15 @@ $(() => {
                 $target.removeAttr('aria-describedby');
                 $contentWrap
                     .on('animationend.tooltip', () => {
+                        $content.removeClass('tooltipContent--' + position);
                         $contentWrap
                             .attr('id', null)
-                            .removeClass('tooltipHideAnim')
+                            .removeClass('tooltipHideAnim' + capitalize(position))
                             .removeClass('isShow')
                             .off('animationend.tooltip')
                             .remove();
                     })
-                    .addClass('tooltipHideAnim');
+                    .addClass('tooltipHideAnim' + capitalize(position));
             }
 
             if (content.substr(0, 1) === '#') {
